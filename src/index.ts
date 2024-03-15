dd4da392fc82169bd555f94a4e221db6b6dc2a8e
JongwooLee <jongwoo328@gmail.com>
2024-03-15 14:17:18 +0900

    express와 관련 종속성, 코드 제거

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
-import express, { NextFunction, Request, Response } from "express";
 import config from "./config";
 import connectDB from "./loader";
 import { TrendService } from "./service";
-const app = express();
+
 dotenv.config();
+
 
 (async () => {
     // 데이터베이스 연결
@@ -14,36 +14,9 @@ dotenv.config();
     await TrendService.getInfoFromSite();
 })();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 // error handler
 interface ErrorType {
     message: string;
     status: number;
 }
-
-app.use(function (
-    err: ErrorType,
-    req: Request,
-    res: Response,
-    next: NextFunction
-) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "real" ? err : {};
-
-    res.status(err.status || 500);
-    res.render("error");
-});
-
-app.listen(config.port, () => {
-    console.log(`
-    -- ${config.nodeEnv}
-    ################################################
-          🛡️  Server listening on port ${config.port} 🛡️
-    ################################################
-  `);
-}).on("error", (err) => {
-    console.error(err);
-    process.exit(1);
-});
