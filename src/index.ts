10676e7efd62f0d800b509db701e4c665a50c1d1
JongwooLee <jongwoo328@gmail.com>
2024-03-15 14:36:47 +0900

    handler 형식으로 코드 수정

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,28 @@ import dotenv from "dotenv";
 import config from "./config";
 import connectDB from "./loader";
 import { TrendService } from "./service";
+import {EventBridgeHandler} from "aws-lambda";
+import {EventBridgeDetail, EventBridgeDetailType, LambdaResult} from "./types/aws/lambda";
 
 dotenv.config();
-
-
-(async () => {
-    // 데이터베이스 연결
-    await connectDB();
-
-    // 데이터베이스 로드가 완료된 후 TrendService.getInfoFromSite 호출
-    await TrendService.getInfoFromSite();
-})();
-
 
 // error handler
 interface ErrorType {
     message: string;
     status: number;
 }
+
+export const handler: EventBridgeHandler<EventBridgeDetailType, EventBridgeDetail, LambdaResult> = async (event, context) => {
+    try {
+        // 데이터베이스 연결
+        await connectDB();
+
+        // 데이터베이스 로드가 완료된 후 TrendService.getInfoFromSite 호출
+        await TrendService.getInfoFromSite();
+
+        return 0;
+    } catch (e) {
+        console.error(e);
+        return 1;
+    }
+};
