9ea5170984b63add70342e45c171551cd0b2b2fa
JongwooLee <jongwoo328@gmail.com>
2024-03-15 17:11:15 +0900

    로컬에서 실행하는 스크립트 추가

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,4 @@ export const handler: EventBridgeHandler<EventBridgeDetailType, EventBridgeDetai
         console.error(e);
     }
 };
+
---
+++ src/local.ts
@@ -0,0 +1,6 @@
+import { handler } from "./index";
+
+// @ts-ignore
+handler().then(() => {
+    process.exit(0);
+});
