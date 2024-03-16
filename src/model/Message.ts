718cbac2faab0c0e538880c2f9e43494411c6fc7
hyejungg <kimhyejung12@naver.com>
2024-03-17 02:30:16 +0900

    스키마 일부 수정

--- src/model/Message.ts
+++ src/model/Message.ts
@@ -7,33 +7,38 @@ const MessageSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        status: {
+            type: String,
+            required: true,
+        },
         messages: [
             {
                 name: {
                     type: String,
-                    required: true,
+                    required: false,
                 },
                 title: {
                     type: String,
-                    required: true,
+                    required: false,
                 },
                 url: {
                     type: String,
-                    required: true,
+                    required: false,
                 },
             },
         ],
     },
     {
         timestamps: true,
+        versionKey: false, // __v(버전 필드) 생성 방지
     }
 );
 
 // timestamps 로 선언 시 생성되는 createdAt, updatedAt
 MessageSchema.index(
     { createdAt: 1 },
-    { expireAfterSeconds: 60 * 60 * 24 * 30 }
-); // 30일
+    { expireAfterSeconds: 60 * 60 * 24 * 180 }
+); // 180일
 
 export default mongoose.model<MessageInfo & mongoose.Document>(
     "Messages",
--- src/model/Site.ts
+++ src/model/Site.ts
@@ -18,6 +18,7 @@ const SiteSchema = new mongoose.Schema(
     },
     {
         timestamps: true,
+        versionKey: false, // __v(버전 필드) 생성 방지
     }
 );
 
