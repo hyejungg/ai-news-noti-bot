4694ab1a1d2fb53d3d104354a0c0384c4deba0b9
hyejungg <kimhyejung12@naver.com>
2024-03-24 18:50:57 +0900

    chrome-aws-lambda 설치하고 puppeteer 설정 변경

--- lambdas/news-scraper/src/types/class/PuppeteerManager.ts
+++ lambdas/news-scraper/src/types/class/PuppeteerManager.ts
@@ -1,33 +1,45 @@
+import config from "@ai-news-noti-bot/common/config";
+import chromium from "chrome-aws-lambda";
 import puppeteer, { Browser, Page } from "puppeteer";
 
 export default class PuppeteerManager {
-  private browser: Browser | null = null;
-  private page: Page | null = null;
+    private browser: Browser | null = null;
+    private page: Page | null = null;
 
-  public async initialize(): Promise<void> {
-    this.browser = await puppeteer.launch({
-      headless: false, // 브라우저 창을 보이게 설정
-      executablePath: puppeteer.executablePath(),
-    });
-    this.page = await this.browser.newPage();
-    await this.page.setViewport({
-      width: 1920,
-      height: 1080,
-    });
-  }
-
-  public getPage(): Page {
-    if (!this.page) {
-      throw new Error("Page is not initialized. Call initialize() first.");
+    public async initialize(): Promise<void> {
+        if (config.nodeEnv === "real") {
+            this.browser = await puppeteer.launch({
+                args: chromium.args,
+                defaultViewport: chromium.defaultViewport,
+                executablePath: await chromium.executablePath,
+                headless: chromium.headless,
+            });
+        }
+        this.browser = await puppeteer.launch({
+            headless: false, // 브라우저 창을 보이게 설정
+            executablePath: puppeteer.executablePath(),
+        });
+        this.page = await this.browser.newPage();
+        await this.page.setViewport({
+            width: 1920,
+            height: 1080,
+        });
     }
-    return this.page;
-  }
 
-  public async close(): Promise<void> {
-    if (this.browser) {
-      await this.browser.close();
+    public getPage(): Page {
+        if (!this.page) {
+            throw new Error(
+                "Page is not initialized. Call initialize() first."
+            );
+        }
+        return this.page;
     }
-    this.browser = null;
-    this.page = null;
-  }
+
+    public async close(): Promise<void> {
+        if (this.browser) {
+            await this.browser.close();
+        }
+        this.browser = null;
+        this.page = null;
+    }
 }
