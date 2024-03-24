424fdb7b6eece8285cc77113462dcd2d1542aaba
hyejungg <kimhyejung12@naver.com>
2024-03-24 19:36:29 +0900

    주석 위치 변경

--- lambdas/news-scraper/src/extractor/implementation/ClueitExtractor.ts
+++ lambdas/news-scraper/src/extractor/implementation/ClueitExtractor.ts
@@ -24,11 +24,10 @@ export default class ClueitExtractor implements Extractor {
     const today = new Date();
     const dayOfWeek = today.getDay(); // 일요일이 0, 월요일이 1, ..., 토요일이 6
 
+    // 클루잇은 수요일에만 업로드 되어서 수요일에만 가져오도록 수정
     if (dayOfWeek !== 3) {
       return [];
     }
-
-    // 클루잇은 수요일에만 업로드 되어서 수요일에만 가져오도록 수정
 
     const apiPath = "https://clueit.substack.com/api/v1/archive";
     const paramsData = {
