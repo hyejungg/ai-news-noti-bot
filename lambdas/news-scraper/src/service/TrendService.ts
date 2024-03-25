7c9fab3e5c9f71c941f5359e24bf7a482affaba0
hyejungg <kimhyejung12@naver.com>
2024-03-25 21:25:38 +0900

    예외처리 수정

--- lambdas/news-scraper/src/service/TrendService.ts
+++ lambdas/news-scraper/src/service/TrendService.ts
@@ -5,7 +5,7 @@ const getInfoFromSite = async () => {
   const sites = await Site.find({ verified: true });
   console.log(`sites: ${sites}`);
 
-  if (!sites) {
+  if (!sites || sites.length === 0) {
     console.error("site 정보를 가져오는 것을 실패했습니다!");
     return;
   }
