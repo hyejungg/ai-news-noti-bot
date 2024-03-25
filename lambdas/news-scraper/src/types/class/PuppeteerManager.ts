ddd54df2a10203483541304ea680db8227884458
JongwooLee <jongwoo328@gmail.com>
2024-03-25 23:09:57 +0900

    config.nodeEnv 체크하는 구문 삭제

--- lambdas/news-scraper/src/types/class/PuppeteerManager.ts
+++ lambdas/news-scraper/src/types/class/PuppeteerManager.ts
@@ -1,4 +1,4 @@
-import config from "@ai-news-noti-bot/common/config";
+// import config from "@ai-news-noti-bot/common/config";
 import chromium from "chrome-aws-lambda";
 import puppeteer, { Browser, Page } from "puppeteer-core";
 
@@ -8,27 +8,27 @@ export default class PuppeteerManager {
 
   public async initialize(): Promise<void> {
     console.log("initialize puppeteer");
-    if (config.nodeEnv === "real") {
-      console.log("real");
-      this.browser = await puppeteer.launch({
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        headless: chromium.headless,
-      });
-    } else {
-      console.log("dev");
-      this.browser = await puppeteer.launch({
-        args: [
-          "--no-sandbox",
-          "--disable-setuid-sandbox",
-          "--disable-dev-shm-usage",
-          "--disable-gpu",
-        ],
-        headless: false, // 브라우저 창을 보이게 설정
-        executablePath: puppeteer.executablePath(),
-      });
-    }
+    console.log("real");
+    this.browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+    // if (config.nodeEnv === "real") {
+    // } else {
+    //   console.log("dev");
+    //   this.browser = await puppeteer.launch({
+    //     args: [
+    //       "--no-sandbox",
+    //       "--disable-setuid-sandbox",
+    //       "--disable-dev-shm-usage",
+    //       "--disable-gpu",
+    //     ],
+    //     headless: false, // 브라우저 창을 보이게 설정
+    //     executablePath: puppeteer.executablePath(),
+    //   });
+    // }
     console.log("initialize puppeteer 2");
     this.page = await this.browser.newPage();
     console.log("initialize puppeteer 3");
