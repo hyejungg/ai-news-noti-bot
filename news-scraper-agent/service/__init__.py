eb45515b3d2a7dc45e8fa7ae61bc98e47147711b
hyejungg <kimhyejung12@naver.com>
2024-10-20 16:54:25 +0900

    사이트, 메시지 정보 가져오는 함수 추가

---
+++ news-scraper-agent/service/__init__.py
@@ -0,0 +1,4 @@
+from .site_service import get_sites
+from .message_service import get_messages
+
+__all__ = ['get_sites', 'get_messages']
---
+++ news-scraper-agent/service/site_service.py
@@ -0,0 +1,15 @@
+from typing import List, Dict, Any
+from models import Site
+from graph import State
+
+# site 정보를 가져와 node 의 상태 중 하나로 추가
+def get_sites(state: State) -> State:
+    sites = list(Site.objects(verified=True))  # QuerySet을 리스트로 변환
+    print("hi")
+    print(f"sites: {sites}")
+
+    # 새로운 딕셔너리를 생성하여 기존 상태를 복사하고 sites를 추가
+    new_state = dict(state)
+    new_state["sites"] = sites
+
+    return new_state
