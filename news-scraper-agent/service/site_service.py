93e84988cd6dc6f6035429eed759b9d36fc2a9c4
hyejungg <kimhyejung12@naver.com>
2024-10-20 17:44:43 +0900

    디버깅용 print 삭제

--- news-scraper-agent/service/site_service.py
+++ news-scraper-agent/service/site_service.py
@@ -4,7 +4,6 @@ from graph import State
 # site 정보를 가져와 node 의 상태 중 하나로 추가
 def get_sites(state: State) -> State:
     sites = list(Site.objects(verified=True))  # QuerySet을 리스트로 변환
-    print("hi")
     print(f"sites: {sites}")
 
     # 새로운 딕셔너리를 생성하여 기존 상태를 복사하고 sites를 추가
