d7ba0b70c1f7e9bc8da419948576e2b22a492fcb
hyejungg <kimhyejung12@naver.com>
2024-10-20 16:54:41 +0900

    그래프 이미지 저장, kst 시간으로 변환하는 유틸 함수 추가

--- news-scraper-agent/utils/time_utils.py
+++ news-scraper-agent/utils/time_utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import pytz
 
-def datetime_kst():
+def get_datetime_kst():
     utc_now = datetime.utcnow()
     kst = pytz.timezone('Asia/Seoul')
     kst_now = utc_now.replace(tzinfo=pytz.utc).astimezone(kst)
