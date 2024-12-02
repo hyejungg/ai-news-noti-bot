6fbd1ae2885f3d73923f1bcbcd1521b27cd5e66e
hyejungg <kimhyejung12@naver.com>
2024-12-02 23:05:19 +0900

    local 실행을 위한 __name__ 추가

--- news-scraper-agent/main.py
+++ news-scraper-agent/main.py
@@ -1,31 +1,23 @@
-from config.log import logger
 from graph.build_graph import build_graph
-from graph.state import State
 from loader.connect import connect_db
 
 
-# from utils import get_graph_image
-
-
-def main():
+def main() -> None:
     connect_db()
+    from graph.state import State
+
     initial_state: State = State(
         sites=[],
         parallel_result={},
     )
     graph = build_graph(initial_state)
     #     get_graph_image(graph)
-    return graph.invoke(initial_state)
+    graph.invoke(initial_state)
+
+
+def lambda_handler(event, context) -> None:
+    main()
 
 
 if __name__ == "__main__":
-    result = main()
-    logger.info(result)
-# FIXME 자세한 로그를 보기 위해 우선 주석
-#     try:
-#         result = main()
-#         logger.info(result)
-#         exit(0)  # 성공적으로 종료
-#     except Exception as e:
-#         logger.error(f"An error occurred: {e}")
-#         exit(1)  # 오류로 인한 종료
+    main()
