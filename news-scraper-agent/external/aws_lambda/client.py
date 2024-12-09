5f024831f473a7304ecb769f7787019fd70bf2a0
JongwooLee <jongwoo328@gmail.com>
2024-12-09 23:12:16 +0900

    retry를 어노테이션으로 사용

--- news-scraper-agent/external/aws_lambda/client.py
+++ news-scraper-agent/external/aws_lambda/client.py
@@ -1,9 +1,23 @@
 import time
 
 import boto3
-from tenacity import retry, wait_fixed, stop_after_attempt
+from tenacity import retry, wait_fixed, stop_after_attempt, RetryCallState
 
 from config.log import NewsScraperAgentLogger
+
+
+def log_before_retry(max_retry: int):
+    def log_before(retry_state: RetryCallState):
+        lambda_invoker: LambdaInvoker = retry_state.args[
+            0
+        ]  # invoke 함수의 첫번째 파라미터 (self)
+        logging_name: str = retry_state.kwargs.get("logging_name", "")
+        if retry_state.attempt_number > 1:
+            lambda_invoker.logger.info(
+                f"Retrying Invoke lambda {logging_name}... {retry_state.attempt_number - 1}/{max_retry - 1}"
+            )
+
+    return log_before
 
 
 class LambdaInvoker:
@@ -11,33 +25,22 @@ class LambdaInvoker:
         self.client = boto3.client("lambda", region_name="ap-northeast-2")
         self.logger = NewsScraperAgentLogger(self.__class__.__name__)
 
-        self.max_retry = 3  # 초
-        self.retry_interval = 3  # 초
-        self.invoke = retry(
-            wait=wait_fixed(self.retry_interval),
-            stop=stop_after_attempt(self.max_retry),
-            before=self.__log_before_retry(),
-        )
-
+    @retry(
+        wait=wait_fixed(wait=3),  # 초
+        stop=stop_after_attempt(max_attempt_number=3),
+        before=log_before_retry(max_retry=3),
+    )
     def invoke(
         self,
         logging_name: str = None,
         **kwargs,
     ):
         name = logging_name if logging_name else "Lambda invocation"
-
         start = time.time()
         self.logger.info(f"Started {name} ")
+
         response = self.client.invoke(**kwargs)
+
         end = time.time()
         self.logger.info(f"Finished {name} ({end - start:.2f}s)")
         return response
-
-    def __log_before_retry(self):
-        def log_before(retry_state):
-            if retry_state.attempt_number > 1:  # 첫 시도는 로깅하지 않음
-                self.logger.info(
-                    f"Retrying Invoke lambda... {retry_state.attempt_number - 1}/{self.max_retry - 1}"
-                )
-
-        return log_before
