84ee5158e92ff4ee19dbd95710585e67ed6a407a
hyejungg <kimhyejung12@naver.com>
2025-02-08 22:16:53 +0900

    _id 필드 지워보고 실행

--- news-scraper-agent/models/site.py
+++ news-scraper-agent/models/site.py
@@ -4,7 +4,6 @@ from mongoengine import (
     ListField,
     BooleanField,
     DateTimeField,
-    ObjectIdField,
 )
 from pydantic import BaseModel
 from typing import Optional
@@ -12,7 +11,7 @@ from utils.time_utils import get_datetime_kst
 
 
 class Site(Document):
-    _id = ObjectIdField(required=True, db_field="_id")  # _id 필드 추가
+    # _id = ObjectIdField(required=True, db_field="_id")  # _id 필드 추가
     name = StringField(required=True, db_field="name")
     url = StringField(required=True, db_field="url")
     keywords = ListField(StringField(), required=False, db_field="keywords")
