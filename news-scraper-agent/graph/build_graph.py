b2a33c793c7e7e03d47c9afc3f45675b9b73b755
김혜정 (hyejung kim) <68772751+hyejungg@users.noreply.github.com>
2026-04-01 19:27:25 +0900

    Fix/#121 모델을 5.4 mini로 변경하고 mcp/에이전트와 관련된 프롬프트 추가 (#126)
    * 5.2 nano 로 모델 변경
    
    * 프롬프트에 에이전트, mcp 관련 내용 추가
    
    * mini로 변경

--- news-scraper-agent/graph/build_graph.py
+++ news-scraper-agent/graph/build_graph.py
@@ -24,9 +24,9 @@ fake_responses = [
 
 def create_crawl_filter_sequence(site: SiteDto) -> Callable[[State], SiteState]:
     html_parser_agent = HtmlParserAgent(site=site)
-    crawling_agent = CrawlingAgent(ChatOpenAI(model="gpt-4o"), site=site)
-    filtering_agent = FilteringAgent(ChatOpenAI(model="gpt-4o"), site=site)
-    sorting_agent = SortingAgent(ChatOpenAI(model="gpt-4o"), site=site)
+    crawling_agent = CrawlingAgent(ChatOpenAI(model="gpt-5.4-mini"), site=site)
+    filtering_agent = FilteringAgent(ChatOpenAI(model="gpt-5.4-mini"), site=site)
+    sorting_agent = SortingAgent(ChatOpenAI(model="gpt-5.4-mini"), site=site)
 
     def process_site(state: State) -> SiteState:
         initial_site_state = SiteState(
